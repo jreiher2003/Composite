90ee2863b65e5e706a42bd8ca673cca4fdb6de53
Jeff Reiher <jreiher2003@yahoo.com>
2016-03-07 20:59:13 -0500

    Change tabs to spaces

--- app/models.py
+++ app/models.py
@@ -3,23 +3,23 @@ from app import db
 
 
 class AsciiArt(db.Model):
-	
-	__tablename__ = 'asciiart'
+    
+    __tablename__ = 'asciiart'
 
-	id = db.Column(db.Integer, primary_key=True)
-	title = db.Column(db.String)
-	art = db.Column(db.String)
-	created = db.Column(db.Date, default=datetime.datetime.now())
-	lon = db.Column(db.Float)
-	lat = db.Column(db.Float)
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    art = db.Column(db.String)
+    created = db.Column(db.Date, default=datetime.datetime.now())
+    lon = db.Column(db.Float)
+    lat = db.Column(db.Float)
 
-	def __init__(self, title, art):
-		self.title = title
-		self.art = art
-		
-	@property 
-	def format_date(self):
-		return '{dt:%A} {dt:%B} {dt.day}, {dt.year}'.format(dt=self.created)
+    def __init__(self, title, art):
+        self.title = title
+        self.art = art
+        
+    @property 
+    def format_date(self):
+        return '{dt:%A} {dt:%B} {dt.day}, {dt.year}'.format(dt=self.created)
 
-	def __repr__(self):
-		return '<title>: {}'.format(self.title)
+    def __repr__(self):
+        return '<title>: {}'.format(self.title)
