fc9209f69bd4e903192675361a701e9f992b5538
Jeff Reiher <jreiher2003@yahoo.com>
2016-03-08 14:21:58 -0500

    Add PRESERVE_CONTEXT_ON_EXCEPTION to test class to fix flask bug in test suite

--- config.py
+++ config.py
@@ -10,6 +10,7 @@ class TestConfig(BaseConfig):
     DEBUG = True
     TESTING = True
     WTF_CSRF_ENABLED = False
+    PRESERVE_CONTEXT_ON_EXCEPTION = False
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
 
 class DevelopmentConfig(BaseConfig):
