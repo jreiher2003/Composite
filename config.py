a9f989373e7c102684cc47e64f4a283922e07b03
Jeff Reiher <jreiher2003@yahoo.com>
2016-07-14 13:31:04 -0400

    Add flask-cache to views -simple config

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ class BaseConfig(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
     SECRET_KEY = os.environ["SECRET_KEY"]
+    CACHE_TYPE = "simple"
 
 class TestConfig(BaseConfig):
     DEBUG = True
